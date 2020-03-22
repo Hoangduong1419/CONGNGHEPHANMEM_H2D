47a74dc37c1ab9f8e4f6a0e7243d8b71246c22d7
Hoangduong1419 <56326913+Hoangduong1419@users.noreply.github.com>
2020-03-22 17:59:33 +0700

    Update Scene2.js

--- my-game/Scene2.js
+++ my-game/Scene2.js
@@ -1,4 +1,6 @@
-
+let _const = 115;
+let ballNumber = 6;
+let status = 0;
 
 class Scene2 extends Phaser.Scene{
 
@@ -9,37 +11,59 @@ class Scene2 extends Phaser.Scene{
     create(){
         this.background = this.add.image(0, 0, "Background1").setOrigin(0, 0);
 
-        //this.animationOfBall = this.add.sprite(145, 10, "animationOfBall");
-
-        // this.anims.create({
-        //     key: "animation_ball",
-        //     frames: this.anims.generateFrameNumbers("animationOfBall"),
-        //     frameRate: 100,
-        //     repeat: -1,
-        // });
-
-        let _const = 115;
-        let ballNumber = 6;
+        var arr = new Array("redBall0");
         for(var i = 0; i < ballNumber; i++){
-            this.redBall = this.add.image(_const += 35, 10, "redBall").setOrigin(0, 0);
+            arr[i] = this.add.image(_const += 35, 10, "redBall0").setOrigin(0, 0);
         }
 
-        var text = this.add.text(350, 150, "Click Above " + "\n" + "The Strange Creature", {font: "25px Arial", fill: "black"});
+        var text1 = this.add.text(350, 150, "Click Above " + "\n" + "The Strange Creature", {font: "25px Arial", fill: "black"});
+        this.clickCountText = this.add.text(0, 150, "Result", {font: "25px Arial", fill: "black"});
+        //button text;
+        const clickButton = this.add.text(200, 80, "Above", {font: "25px Arial", fill: "black" })
+            .setInteractive()
+            .on('pointerdown', () => this.moveBall(arr[ballNumber - 1]));
 
-        // var button = this.add.sprite(0, 100, "button").setOrigin(0, 0);
-        // button.inputEnabled = true;
-        // button.events.onInputDown.add(action(), this);
-        //
-        // function action(){
-        //     text.text = 'correct';
-        // }
-        // let agrid = new AlignGrid({scene:this, row: 11, cols: 11});
-        // agrid.showNumbers();
-        //agrid.placeAtIndex()
+        const clickButton1 = this.add.text(200, 280, "Below", {font: "25px Arial", fill: "black" })
+            .setInteractive()
+            .on('pointerdown', () => this.moveBall1(arr[ballNumber - 1]));
+
+    }
+    
+     moveBall(ball){
+        if(status == 0){
+            this.clickCountText.setText("Correct!");
+            this.text1.setText( "Click Below " + "\n" + "The Strange Creature");
+            status = 1;
+            ball.x += 2000;
+            ballNumber--;
+            if(ballNumber == 0){
+                this.text2 = this.add.text(250, 10, "Huan Ngu Lon", {font: "25px Arial", fill: "red" })
+            }
+        }
+        else{
+            this.clickCountText.setText("Wrong!");
+            this.text1.setText("Below Below co ma!");
+        }
+
+
+    }
+    moveBall1(ball){
+        if(status == 1){
+            ball.x += 2000;
+            this.clickCountText.setText("Correct!");
+            this.text1.setText( "Click Above " + "\n" + "The Strange Creature");
+            status = 0;
+            ballNumber--;
+            if(ballNumber == 0){
+                this.text2 = this.add.text(250, 10, "Huan Ngu Lon", {font: "25px Arial", fill: "red" })
+            }
+        }
+        else{
+            this.clickCountText.setText("Wrong!");
+            this.text1.setText("Above Above co ma!");
+        }
     }
 
     update(){
-        //this.moveBall(this.redBall, 2);
     }
-
 }
